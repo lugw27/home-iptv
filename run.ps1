66c26fa1c4a2f6a14d23821267053203149a4cab
lugw27 <754492986@qq.com>
2024-12-31 14:15:56 +0800

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,8 @@
+$file='iptv.m3u'
+$new_file="iptv-udpate.m3u"
+$url='https://www.mytvsuper.com.mp/m3u/Live.m3u'
+
+$r=Invoke-RestMethod -Method get -Uri $url
+
+Copy-Item -Path $file -Destination $new_file -Force
+$r|Add-Content -Path $new_file -Force
