2bf80a60fb84f53035e491caadf6d31dd3d05012
lugw27 <754492986@qq.com>
2024-12-31 14:23:03 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,7 +2,10 @@ $file='iptv.m3u'
 $new_file="iptv-udpate.m3u"
 $url='https://www.mytvsuper.com.mp/m3u/Live.m3u'
 
+"started"
 $r=Invoke-RestMethod -Method get -Uri $url
 
 Copy-Item -Path $file -Destination $new_file -Force
 $r|Add-Content -Path $new_file -Force
+
+"end"
