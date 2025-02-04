5583cfe66ef884304fc16c1920ceae8269778f7f
lugw27 <754492986@qq.com>
2025-02-04 10:00:46 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,11 +1,16 @@
 $file='iptv.m3u'
 $new_file="iptv-udpate.m3u"
 
-$urls="https://live.hkdvb.com/hls/playlist.m3u?token=560806621546352",'https://www.mytvsuper.com.mp/m3u/Live.m3u'
+$urls=[ordered]@{
+    "hkdvb"="https://live.hkdvb.com/hls/playlist.m3u?token=560806621546352"
+    "judy"="https://www.mytvsuper.com.mp/m3u/Live.m3u"
+}
 
 "started"
 Copy-Item -Path $file -Destination $new_file -Force
-foreach ($url in $urls){
+foreach ($k in $urls.keys){
+  $url=$urls[$k]
+  
   $r=Invoke-RestMethod -Method get -Uri $url
   if ($url.contains('live.hkdvb.com')){
     #修改group-title
