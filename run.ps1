ae4b8f8bf16975ff4eea360bd0c73ab8ddcf4802
lugw27 <754492986@qq.com>
2025-02-24 19:13:47 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -5,34 +5,41 @@ $new_file="iptv-udpate.m3u"
 
 $urls=[ordered]@{
     "hkdvb"="https://live.hkdvb.com/hls/playlist.m3u?token=560806621575418"
-    #"judy"="https://video.mytvsuper.xyz/playlist/zong-he-ding-yue.m3u?token=560806621406341"
+    "suxuang-港澳台"="./suxuang-ipv4.m3u"
 }
 
 "started"
 Copy-Item -Path $file -Destination $new_file -Force
 foreach ($k in $urls.keys){
   write-host $k
-
   $url=$urls[$k]
 
-  try{
-    $r=Invoke-RestMethod -Method get -Uri $url -ErrorAction SilentlyContinue
-    write-host "Invoke-RestMethod 成功"
-    $r|Out-File "./cache/$k.cache" -force
+  if (Test-Path $url){
+    #本地文件
+    $r=Get-Content -Path $url -Encoding UTF8
   }
-  catch{
-    write-host "Invoke-RestMethod 失败"
-    $r=Get-Content -Path "./cache/$k.cache" -Raw
+  else{
+    #url
+    try{
+      $r=Invoke-RestMethod -Method get -Uri $url -ErrorAction SilentlyContinue
+      write-host "Invoke-RestMethod 成功"
+      $r|Out-File "./cache/$k.cache" -force
+    }
+    catch{
+      write-host "Invoke-RestMethod 失败"
+      $r=Get-Content -Path "./cache/$k.cache" -Raw
+    }
+
+    if ($url.contains('live.hkdvb.com')){
+      #修改group-title
+      $r=$r -replace 'group-title=\".*\",','group-title="hkdvb",'
+      #替换"Jade "
+      $r=$r -replace 'Jade ',''
+      #替换"[直播]"
+      $r=$r -replace '\[直播\]',''
+    }
   }
 
-  if ($url.contains('live.hkdvb.com')){
-    #修改group-title
-    $r=$r -replace 'group-title=\".*\",','group-title="hkdvb",'
-    #替换"Jade "
-    $r=$r -replace 'Jade ',''
-    #替换"[直播]"
-    $r=$r -replace '\[直播\]',''
-  }
   $r|Add-Content -Path $new_file -Force
 }
 
