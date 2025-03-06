ffff5354473b6973defbfb35d41bf42fd55af742
lugw27 <754492986@qq.com>
2025-03-06 14:04:22 +0800

    Update aktv.ps1

--- aktv.ps1
+++ aktv.ps1
@@ -2,6 +2,9 @@ $url = "http://aktv.space/live.m3u"
 $outputFile = "./aktv.m3u"
 
 $r=Invoke-RestMethod -Method Get -Uri $url
+#频道名称加上[aktv]标识
+$r = $r -replace ',',',[aktv]'
+#加上组名称
 $r = $r -replace '#EXTINF:-1','#EXTINF:-1 group-title="aktv"'
 
 $r | Out-File -FilePath $outputFile -Force -Encoding utf8
