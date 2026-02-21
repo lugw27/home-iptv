a853123c6a3192a0bb259d85c32570adac9ab109
lugw27 <754492986@qq.com>
2026-02-21 20:06:46 +0800

    Update SearchIPTV.ps1

--- SearchIPTV.ps1
+++ SearchIPTV.ps1
@@ -1,5 +1,5 @@
 $url="https://d2vldammsgprja.cloudfront.net/4e74cd22-b28e-4cdf-878e-5bb480321801/SearchIPTV.m3u"
-$outputFile=./SearchIPTV.m3u
+$outputFile="./SearchIPTV.m3u"
 
 $r=Invoke-RestMethod $url 
 $r|Out-File $outputFile -Encoding utf8
